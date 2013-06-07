3a7dd8c7795f47e1bdf91bb7d831d4e8bb04e47b
Scott Rossillo <scott@rossillo.net>
2013-06-07 13:38:04 -0400

    Find cars by automaker using simple $in criteria

--- src/main/java/net/rossillo/car/repository/CarRepositoryImpl.java
+++ src/main/java/net/rossillo/car/repository/CarRepositoryImpl.java
@@ -1,12 +1,10 @@
 package net.rossillo.car.repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import net.rossillo.car.domain.Automaker;
 import net.rossillo.car.domain.Car;
 
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -19,15 +17,7 @@ public final class CarRepositoryImpl implements CustomCarRepository {
 
 	@Override
 	public List<Car> findCarsByAutomakers(Automaker... makers) {
-		
-		final List<ObjectId> idList = new ArrayList<ObjectId>(makers.length);
-		
-		for(final Automaker maker : makers) {
-			idList.add(new ObjectId(maker.getId()));
-		}
-		
-		Query q = new Query(new Criteria("automaker.$id").in(idList));
-		
+		Query q = new Query(new Criteria("automaker").in((Object[]) makers));
 		return template.find(q, Car.class);
 	}
 
